fba90d9c1cc99c288d774f054d0c95977223fbad
Colin <colin@thinkr.fr>
2017-07-05 18:13:47 +0200

    changed erreur to error

--- tests/testthat/test-erreur_correction_propostion.R
+++ tests/testthat/test-erreur_correction_propostion.R
@@ -1,6 +1,6 @@
-context("erreur_correction_propostion")
+context("error_correction_propostion")
 
-test_that("erreur_correction_propostion works", {
-  expect_equal(erreur_correction_propostion("view")[1],"View")
-  expect_equal(erreur_correction_propostion("sl")[1],"ls")
+test_that("error_correction_propostion works", {
+  expect_equal(error_correction_propostion("view")[1],"View")
+  expect_equal(error_correction_propostion("sl")[1],"ls")
 })
