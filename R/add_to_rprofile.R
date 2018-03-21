7b5a4c91aec0b7245f508087a4b871daf07ae4cb
privefl <florian.prive.21@gmail.com>
2018-03-21 11:31:10 +0100

    silent loading + global option

--- R/add_to_rprofile.R
+++ R/add_to_rprofile.R
@@ -1,15 +1,23 @@
 #' Add library(fcuk) to the .Rprofile file
 #' 
 #' After calling this function, fcuk will be launched everytime you launch
-#' your current R project.
+#' your current R project (or any project if you use option `global = TRUE`).
+#' 
+#' @param global Whether to add it to the global .Rprofile (`TRUE`) or just to 
+#'   the  .Rprofile file of your current project (`FALSE`, the default).
 #' 
 #' @export
 #' @examples
 #' fcuk::add_fcuk_to_rprofile()
-add_fcuk_to_rprofile <- function(){
+add_fcuk_to_rprofile <- function(global = FALSE) {
   
-  if ((file.exists(".Rprofile") &&
-      !any(readLines(".Rprofile")=="library(fcuk)"))|!file.exists(".Rprofile")){
-      cat("\nlibrary(fcuk)\n",file = ".Rprofile",append = TRUE)
+  file <- `if`(global, "~/.Rprofile", ".Rprofile")
+  
+  if (file.exists(file)) {
+    already_there <- any(grepl("(fcuk)", readLines(file), fixed = TRUE))
+    if (already_there) return(invisible(NULL))
   }
+  
+  to_add <- "\nif (interactive()) {\n  suppressMessages(require(fcuk))\n}\n"
+  cat(to_add, file = file, append = TRUE)
 }
