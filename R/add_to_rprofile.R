999b4312fc59507083e35c07e664580de78e18ef
Vincent <vincent@thinkr.fr>
2018-04-16 22:10:49 +0200

    add done

--- R/add_to_rprofile.R
+++ R/add_to_rprofile.R
@@ -9,15 +9,19 @@
 #' @export
 #' @examples
 #' fcuk::add_fcuk_to_rprofile()
-add_fcuk_to_rprofile <- function(global = FALSE) {
+add_fcuk_to_rprofile <- function(global = TRUE) {
   
   file <- `if`(global, "~/.Rprofile", ".Rprofile")
   
   if (file.exists(file)) {
     already_there <- any(grepl("(fcuk)", readLines(file), fixed = TRUE))
-    if (already_there) return(invisible(NULL))
+    if (already_there) {
+      
+      return(invisible(NULL))}
   }
   
   to_add <- "\nif (interactive()) {\n  suppressMessages(require(fcuk))\n}\n"
   cat(to_add, file = file, append = TRUE)
+  done("Adding fcuk to ",file)
+  
 }
---
+++ R/check_mark.R
@@ -0,0 +1,13 @@
+# from usethis https://github.com/r-lib/usethis
+cat_line <- function(...) {
+  cat(..., "\n", sep = "")
+}
+
+bullet <- function(lines, bullet) {
+  lines <- paste0(bullet, " ", lines)
+  cat_line(lines)
+}
+
+done <- function(...) {
+  bullet(paste0(...), bullet = crayon::green(clisymbols::symbol$tick))
+}
