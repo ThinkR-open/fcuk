49e4e27c345d9bb1a36113dddf27a4c4ad4e3642
statnmap <statnmap@users.noreply.github.com>
2019-05-15 09:06:33 +0200

    update pkgdown template. Add pkg level doc

--- R/doc_package.R
+++ R/doc_package.R
@@ -1,9 +1,8 @@
-#' fcuk
-#' 
-#' Automatically suggests a correction when a typo occurs.
-#' 
-#' @name fcuk-package
-#' @aliases fcuk-package fcuk
-#' @docType package
-#' @author vincent <vincent@@thinkr.fr>
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
 NULL
